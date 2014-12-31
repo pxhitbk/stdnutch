2c438c2d34109b64d40a407556cd2037b1c46f20
Hung Pham <pxhitbk@gmail.com>
2014-12-31 15:11:14 +0700

    update

--- src/java/org/apache/nutch/test/CrawlerTest.java
+++ src/java/org/apache/nutch/test/CrawlerTest.java
@@ -1,5 +1,6 @@
 package org.apache.nutch.test;
 import java.io.IOException;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,19 +16,52 @@ import org.apache.nutch.crawl.CrawlDatum;
 import org.apache.nutch.crawl.CrawlDb;
 import org.apache.nutch.crawl.Generator;
 import org.apache.nutch.crawl.Injector;
+import org.apache.nutch.fetcher.Fetcher;
+import org.apache.nutch.protocol.Content;
 import org.apache.nutch.util.NutchConfiguration;
+import org.mortbay.jetty.Server;
 
 public class CrawlerTest extends Configured implements Tool {
 	public static final String CRAWL_DB = "crawldb";
 	public static final String URLS = "urls";
 	public static final String SEGMENT = "segment";
 	
+	Server server;
+	
 	Injector injector = new Injector();
 	Generator generator = new Generator();
+	Fetcher fetcher = new Fetcher();
 	
 	public static void main(String[] args) throws Exception {
 		int res = ToolRunner.run(NutchConfiguration.create(), new CrawlerTest(), args);
 		System.exit(res);
+	}
+	
+	@Override
+	public int run(String[] args) throws Exception {
+		//Injector
+		injector.run(new String[] {CRAWL_DB, URLS});
+		List<String> crawlDb = readCrawldb();
+		TestUtils.printCrawlDb(crawlDb);
+		
+		//Generator
+		Path[] generatedSegment = generateFetchlist(2, getConf(), false);
+		
+		Path fetchlist = new Path(new Path(generatedSegment[0],
+		        CrawlDatum.GENERATE_DIR_NAME), "part-00000");
+
+	    ArrayList<URLCrawlDatum> contents = readContents(fetchlist);
+	    TestUtils.printGeneratedSegment(contents);
+	    
+	    //Fetcher
+	    fetcher.setConf(getConf());
+	    boolean parser = getConf().getBoolean("fetcher.parse", true);
+	    System.err.println("fetcher.parse: " + parser);
+	    
+	    fetcher.fetch(generatedSegment[0], 1);
+	    List<FetchedData> fetchedContents = readFetchData(generatedSegment[0]);
+	    
+		return 0;
 	}
 	
 	private List<String> readCrawldb() throws IOException{
@@ -50,6 +84,37 @@ public class CrawlerTest extends Configured implements Tool {
 
 	    return read;
 	  }
+	
+	private List<FetchedData> readFetchData(Path generatedSegment) {
+		//verify content
+	    List<FetchedData> contents = null;
+		try {
+			Path content=new Path(new Path(generatedSegment, Content.DIR_NAME),"part-00000/data");
+			SequenceFile.Reader reader=new SequenceFile.Reader(FileSystem.get(getConf()), content, getConf());
+			
+			ArrayList<String> handledurls=new ArrayList<String>();
+			contents = new ArrayList<FetchedData>();
+			READ_CONTENT:
+			  do {
+			  Text key=new Text();
+			  Content value=new Content();
+			  FetchedData fd = new FetchedData(key, value);
+			  contents.add(fd);
+			  if(!reader.next(key, value)) break READ_CONTENT;
+			  String contentString=new String(value.getContent());
+			  if(contentString.indexOf("Nutch fetcher test page")!=-1) { 
+			    handledurls.add(key.toString());
+			  }
+			} while(true);
+
+			reader.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    
+	    return contents;
+	}
 
 	/**
 	   * Read contents of fetchlist.
@@ -76,26 +141,6 @@ public class CrawlerTest extends Configured implements Tool {
 	    return l;
 	  }
 	
-	@Override
-	public int run(String[] args) throws Exception {
-		//Injector
-		injector.run(new String[] {CRAWL_DB, URLS});
-		
-		//Generator
-		Path generatedSegment = generateFetchlist(2, getConf(), false);
-		
-		Path fetchlist = new Path(new Path(generatedSegment,
-		        CrawlDatum.GENERATE_DIR_NAME), "part-00000");
-
-	    ArrayList<URLCrawlDatum> contents = readContents(fetchlist);
-	    printGeneratedSegment(contents);
-	    
-	    //Fetcher
-	    
-	    
-		return 0;
-	}
-	
 	/**
 	   * Generate Fetchlist.
 	   * @param numResults number of results to generate
@@ -103,28 +148,17 @@ public class CrawlerTest extends Configured implements Tool {
 	   * @return path to generated segment
 	   * @throws IOException
 	   */
-	  private Path generateFetchlist(int numResults, Configuration config,
+	  private Path[] generateFetchlist(int numResults, Configuration config,
 	      boolean filter) throws IOException {
 	    // generate segment
 	    Generator g = new Generator(config);
 	    Path[] generatedSegment = g.generate(new Path(CRAWL_DB), new Path(SEGMENT), -1, numResults,
 	        Long.MAX_VALUE, filter, false);
 	    if (generatedSegment==null) return null;
-	    return generatedSegment[0];
+	    return generatedSegment;
 	  }
 
 	
-	void printCrawlDb() throws IOException {
-		List<String> crldb = readCrawldb();
-		for(String s: crldb) {
-			System.out.println(s);
-		}
-	}
 	
-	void printGeneratedSegment(List<URLCrawlDatum> contents) throws IOException {
-		for(URLCrawlDatum c: contents) {
-			System.out.println(c.toString());
-		}
-	}
 }
 
---
+++ src/java/org/apache/nutch/test/FetchedData.java
@@ -0,0 +1,20 @@
+package org.apache.nutch.test;
+
+import org.apache.hadoop.io.Text;
+import org.apache.nutch.protocol.Content;
+
+public class FetchedData {
+	private Text key;
+	private Content content;
+	
+	public FetchedData(Text key, Content content) {
+		super();
+		this.key = key;
+		this.content = content;
+	}
+	
+	@Override
+	public String toString() {
+		return "Key: " + key.toString() + "/ content: " + content.toString();
+	}
+}
